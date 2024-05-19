9dd4d023a1aca7a50528ce026aad3e02a70d7179
shafik720 <shafikrasel5@gmail.com>
2024-05-19 18:23:28 +0600

    creating navbar

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="lg:max-w-[1200px] mx-auto">
+      <Navbar />
       <h2>Home is here</h2>
     </div>
   );
