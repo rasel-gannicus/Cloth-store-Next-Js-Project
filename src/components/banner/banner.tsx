9576ee3b38ac606eb3040cf3ed6e2718f529dd70
shafik720 <shafikrasel5@gmail.com>
2024-05-19 20:57:34 +0600

    banner making done

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -18,13 +18,13 @@ const Banner = () => {
       </div> */}
 
       {/* --- banner text and buttons --- */}
-      <div className="flex flex-col gap-10 lg:flex-row justify-between items-center w-full px-20 z-20">
-        <div className="banner-left-side flex flex-col gap-5 items-start max-w-60 text-white">
+      <div className="flex flex-col gap-10 md:flex-row justify-between items-center w-full px-20 z-20">
+        <div className="banner-left-side flex flex-col gap-5 items-start max-w-60 text-white banner-left">
             <h1 className="uppercase text-4xl font-bold"> <span className="text-[#65CDF5] ">Men's </span> <br /> fashion</h1>
             <p>Explore the gorgeous collection for Men's here</p>
             <button className="btn bg-[#65CDF5] border-none font-semibold rounded-none text-white text-lg hover:btn-neutral">Shop Now</button>
         </div>
-        <div className="banner-left-side flex flex-col gap-5 items-start max-w-60 text-white">
+        <div className="banner-left-side flex flex-col gap-5 items-start max-w-60 text-white banner-left">
             <h1 className="uppercase text-4xl font-bold"> <span className="text-[#F565B0] ">Women's </span> <br /> fashion</h1>
             <p>Explore the gorgeous collection for Women's here</p>
             <button className="btn bg-[#F565B0] font-semibold rounded-none text-white text-lg border-none hover:btn-neutral">Shop Now</button>
