6cda4b2d3e0defd96a5b2fd5846fec4b47eafea8
shafik720 <shafikrasel5@gmail.com>
2024-05-20 22:35:42 +0600

    created top rated section

--- src/components/for home page/most popular products/MostPopularProducts.tsx
+++ src/components/for home page/most popular products/MostPopularProducts.tsx
@@ -1,26 +1,48 @@
+import { TCloths } from "@/types/types";
 import PopularProductCard from "./Popular Products Card/PopularProductCard";
 
+const MostPopularProducts = async () => {
+  // --- fetching data with ISR method
+  const res = await fetch(
+    "https://server-for-assignment-8.vercel.app/allCloths",
+    {
+      next: {
+        revalidate: 30,
+      },
+    }
+  );
 
-const MostPopularProducts = () => {
-    return (
-        <div className="flex flex-col justify-center items-center py-20 ">
-            <div className="md:flex-row md:justify-between  flex flex-col justify-center items-center w-full">
-                <div className="text-center md:text-left">
-                    <h2 className="md:text-4xl text-3xl font-semibold">Most Popular Products</h2>
-                    <p className="text-gray-500 my-5">See our most rated products here</p>
-                </div>
-                <button className="btn btn-neutral md:btn-lg rounded-full ">View All</button>
-            </div>
-
-            <div className="my-10 md:grid md:grid-cols-4 grid grid-cols-1 gap-y-10">
-                <PopularProductCard />
-                <PopularProductCard />
-                <PopularProductCard />
-                <PopularProductCard />
-                <PopularProductCard />
-            </div>
+  const data = await res.json();
+  //   if (data.length > 0) {
+  //     // Sort the products by ratings in descending order and get the top 3
+  //     const topRatedProducts = [...data]
+  //       .sort((a, b) => b.Ratings - a.Ratings)
+  //       .slice(0, 3);
+  //   }
+  const topRatedProducts = [...data]
+    .sort((a, b) => b.Ratings - a.Ratings)
+    .slice(0, 6);
+  return (
+    <div className="flex flex-col justify-center items-center py-20 ">
+      <div className="md:flex-row md:justify-between  flex flex-col justify-center items-center w-full">
+        <div className="text-center md:text-left">
+          <h2 className="md:text-4xl text-3xl font-semibold">
+            Most Popular Products
+          </h2>
+          <p className="text-gray-500 my-5">See our most rated products here</p>
         </div>
-    );
+        <button className="btn btn-neutral md:btn-lg rounded-full ">
+          View All
+        </button>
+      </div>
+
+      <div className="my-10 md:grid md:grid-cols-4 grid grid-cols-1 gap-y-10">
+        {topRatedProducts.map((item: TCloths) => (
+          <PopularProductCard key={item._id} data={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MostPopularProducts;
--- src/components/for home page/most popular products/Popular Products Card/PopularProductCard.tsx
+++ src/components/for home page/most popular products/Popular Products Card/PopularProductCard.tsx
@@ -1,18 +1,19 @@
 import { TCloths } from "@/types/types";
 
-const PopularProductCard = () => {
+const PopularProductCard = ({data} : {data : TCloths}) => {
   return (
     <div className="md:max-w-80 w-[95%] mx-5">
       <div className="rounded overflow-hidden">
         <img
-          src="https://themewagon.github.io/hexashop/assets/images/men-03.jpg"
+          src={data.Images}
           alt=""
         />
       </div>
       <div className="my-3 mx-3">
-        <h2 className="text-lg font-semibold text-gray-600">Product Title</h2>
+        <h2 className="text-lg font-semibold text-gray-600">{data.Title}</h2>
+        <p className="text-gray-400 text-sm">Ratings : {data.Ratings} </p>
         <div className="flex justify-between items-center my-1">
-            <p className="text-sm">Price : $250 </p>
+            <p className="text-sm">Price : ${data.Price}</p>
             <button className="btn btn-neutral hover:btn-ghost btn-xs rounded-none">Buy Now</button>
         </div>
       </div>
