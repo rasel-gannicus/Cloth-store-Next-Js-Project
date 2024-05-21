bc780f65aa5a8327c17ea89d721f04c32448d26c
shafik720 <shafikrasel5@gmail.com>
2024-05-21 18:15:46 +0600

    successfully data loaded on dashboard in table format

--- src/app/(adminLayout)/layout.tsx
+++ src/app/(adminLayout)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import Sidebar from "@/components/shared/Dashboard Sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Sidebar />
+        <div className="p-4 sm:ml-64">{children}</div>
+      </body>
     </html>
   );
 }
