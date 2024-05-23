83c1509502ef89fb091c8a573c38a59fc598388e
shafik720 <shafikrasel5@gmail.com>
2024-05-23 22:32:22 +0600

    fixed issue in products route

--- src/app/(commonLayout)/products/page.tsx
+++ src/app/(commonLayout)/products/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import ProductCard from "@/components/products/product card/ProductCard";
+import { TCloths } from "@/types/types";
+import React from "react";
 
-const AllProducts = () => {
-    return (
-        <div>
-            <h2>All Products Here</h2>
-        </div>
-    );
+const AllProducts = async () => {
+  //   --- fetching data from server with SSR
+  const res = await fetch(
+    "https://server-for-assignment-8.vercel.app/allCloths",
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await res.json();
+
+  return (
+    <div>
+      <h2 className="text-2xl md:text-4xl font-semibold text-center mb-10">
+        Showing
+        <span className="text-pink-600">All</span>
+        products
+      </h2>
+      <div className="py-5 grid md:grid-cols-3 gap-6 px-1">
+        {data?.length > 0 ? (
+          data?.map((item: TCloths) => (
+            <ProductCard key={item._id} data={item} />
+          ))
+        ) : (
+          <h2>No data found</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AllProducts;
